b2c1b9ac1d5d14f27e3a59e56d103b9b4550eb95
Diyorbek <90106883+iDev07@users.noreply.github.com>
2022-07-25 12:23:07 +0500

    Next js practise

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
-
+import "../styles/globals.css";
+import App from "../components/App";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <App>
+        <Component {...pageProps} />
+      </App>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
